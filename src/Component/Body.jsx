11e97ba105cf1edc742dee7bd6396c89070e9996
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-03-25 23:46:14 +0530

    some styleing changed

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -4,59 +4,63 @@ import Shimmer from "./Shimmer";
 import { RES_API } from "../Utility/Content";
 
 const Body = () => {
-  const [listOfRestaurant, setListOfRestaurant] = useState([]);
-  const [search, setSearch] = useState([]);
-  const [newSearchData, setNewSearchData] = useState([]);
+	const [listOfRestaurant, setListOfRestaurant] = useState([]);
+	const [search, setSearch] = useState([]);
+	const [newSearchData, setNewSearchData] = useState([]);
 
-  const searchBar = () => {
-    const searchdata = listOfRestaurant.filter((e) =>
-      e.info.name.toLowerCase().includes(search.toLowerCase())
-    );
-    setNewSearchData(searchdata);
-  };
-  const filteredList = () => {
-    const newdata = listOfRestaurant.filter((res) => res.info.avgRating > 4);
-    setListOfRestaurant(newdata);
-  };
+	const searchBar = () => {
+		const searchdata = listOfRestaurant.filter((e) =>
+			e.info.name.toLowerCase().includes(search.toLowerCase())
+		);
+		setNewSearchData(searchdata);
+	};
+	const filteredList = () => {
+		const newdata = listOfRestaurant.filter((res) => res.info.avgRating > 4);
+		setListOfRestaurant(newdata);
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-  const fetchData = async () => {
-    const data = await fetch(RES_API);
+	const fetchData = async () => {
+		const data = await fetch(RES_API);
 
-    const json = await data.json();
+		const json = await data.json();
 
-    const resData = await json?.data?.cards[1]?.card?.card?.gridElements
-      ?.infoWithStyle?.restaurants;
+		const resData = await json?.data?.cards[1]?.card?.card?.gridElements
+			?.infoWithStyle?.restaurants;
 
-    setListOfRestaurant(resData);
-    setNewSearchData(resData);
-    console.log(resData);
-  };
+		setListOfRestaurant(resData);
+		setNewSearchData(resData);
+		console.log(resData);
+	};
 
-  return listOfRestaurant.length == 0 ? (
-    <Shimmer />
-  ) : (
-    <div>
-      <div className="filterBtn">
-        <button onClick={filteredList}>Top Rated Restaurant</button>
-        <input
-            type="text"
-            placeholder="Enter Restaurant Name"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <button onClick={searchBar}>Search</button>
-      </div>
-      <div className="main-container">
-        {newSearchData.map((res) => {
-          return <RestaurantsCart key={res.info.id} data={res.info} />;
-        })}
-      </div>
-    </div>
-  );
+	return listOfRestaurant.length == 0 ? (
+		<Shimmer />
+	) : (
+		<div>
+			<div className="filterBtn">
+				<button className="fiteredBtn" onClick={filteredList}>Top Rated Restaurant</button>
+				<div className="inputSec">
+					<input
+						type="text"
+						placeholder="Enter Restaurant Name"
+						value={search}
+						onChange={(e) => setSearch(e.target.value)}
+					/>
+					<button className="searchBar" onClick={searchBar}>
+						Search
+					</button>
+				</div>
+			</div>
+			<div className="main-container">
+				{newSearchData.map((res) => {
+					return <RestaurantsCart key={res.info.id} data={res.info} />;
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default Body;
