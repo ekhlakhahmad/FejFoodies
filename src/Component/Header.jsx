bb7bf36851105f36c5ebe81778062b6c06a7f0b1
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-03-27 23:59:13 +0530

    Added some files

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,37 +1,41 @@
 import React, { useState } from 'react'
-import LOGO from '../Assests/2.png'
+import LOGO from "../Assests/AhmadLOGO.png";
 import { Link } from "react-router-dom";
+import { BsCart4 } from "react-icons/bs";
 
 const Header = () => {
-  const [login, SetLogin] = useState("Login");
-  return (
-    <div className="header-sec">
-      <div className="logo">
-        <img src={LOGO} alt="logo" />
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/gallary">Gallary</Link>
-          </li>
-          <li>Grocery</li>
-          <li>Cart</li>
-          <button
-            onClick={() => {
-              login == "Login" ? SetLogin("Logout") : SetLogin("Login");
-            }}>
-            {login}
-          </button>
-        </ul>
-      </nav>
-    </div>
-  );
+	const [login, SetLogin] = useState("Login");
+	return (
+		<div className="header-sec">
+			<div className="logo">
+				<img src={LOGO} alt="logo" />
+			</div>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+					<li>
+						<Link to="/gallary">Gallary</Link>
+					</li>
+					<li>Grocery</li>
+				</ul>
+			</nav>
+			<div className='rightNav'>
+				<div><BsCart4/></div>
+				<div>DarkMode</div>
+				<button
+					onClick={() => {
+						login == "Login" ? SetLogin("Logout") : SetLogin("Login");
+					}}>
+					{login}
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Header
