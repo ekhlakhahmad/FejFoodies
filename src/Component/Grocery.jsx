04d396d54e0c8633bc46be407afe6842406a5bf8
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-04-21 11:53:51 +0530

    changed in grocerry componenets

--- src/Component/Grocery.jsx
+++ src/Component/Grocery.jsx
@@ -1,11 +1,442 @@
 import React from 'react'
+import { FaRegHeart } from "react-icons/fa6";
+
 
 const Grocery = () => {
   return (
-    <div>
-      This is Grocery page
-    </div>
-  )
+		<div className="flex flex-col items-center  py-10 px-30 gap-5">
+			<h1 className="px-20 py-4 border-2 border-[#039a63] shadow-sm tra rounded-full">
+				Vegatables Section
+			</h1>
+			<div className="flex justify-center flex-wrap gap-5">
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbk0AF8kfFhXL5gPuUmGyDFBhs6G4jg_ib-A&s"
+						alt=""
+					/>
+					<p className="text-center">Beet Root</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://www.bigbasket.com/media/uploads/p/m/10000326_14-fresho-coriander-leaves.jpg?tr=w-1920,q=80"
+						alt=""
+					/>
+					<p className="text-center">Coriander Leaves</p>
+					<div className="flex justify-between">
+						<p>Rs. 120 /-</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://www.bigbasket.com/media/uploads/p/m/10000067_23-fresho-capsicum-green.jpg?tr=w-1920,q=80"
+						alt=""
+					/>
+					<p className="text-center">Shimla Mirch/Green Capsicum</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t4.ftcdn.net/jpg/00/10/34/37/240_F_10343740_3W3izpz69cqamcyyKthcEoCCgP5r77i0.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t4.ftcdn.net/jpg/00/53/59/03/240_F_53590332_GktsdggabKrEv4a3URfSalFHofBoFOqL.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://www.apnikirana.com/thumb/thumb200X200/6e7590e740fa30e53cac0bdff738613c.jpg"
+						alt=""
+					/>
+					<p className="text-center">Hirvi Mirchi/Green Chili</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t4.ftcdn.net/jpg/00/32/09/57/240_F_32095781_CsyOETUW3ksonb7MxH4PzYfu2SPw6Lv8.jpg"
+						alt=""
+					/>
+					<p className="text-center">Patta Gobhi/Cabbage </p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t3.ftcdn.net/jpg/00/41/30/10/240_F_41301053_AbLi3hlosbLajBo7lQdNyfGz9eusxY1x.jpg"
+						alt=""
+					/>
+					<p className="text-center">Aaloo/Potatos</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://www.apnikirana.com/thumb/thumb200X200/d5f755905c1f62962601ef350b1bcd86.jpg"
+						alt=""
+					/>
+					<p className="text-center">Bhendi/Okra/Lady Finger</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://www.apnikirana.com/thumb/thumb200X200/fe7c5c539c54c4fd1a2bdd8db5765b70.jpg"
+						alt=""
+					/>
+					<p className="text-center">Baingan Bharta/Brinjal</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+			</div>
+			<h1 className="px-20 py-4 border-2 border-[#039a63] shadow-sm tra rounded-full">
+				Personal Care Section
+			</h1>
+			<div className="flex justify-center flex-wrap gap-5">
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t3.ftcdn.net/jpg/03/37/08/60/240_F_337086015_hxXQVScf64f0PLQKCo9BIpSYjzbybsw8.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t3.ftcdn.net/jpg/00/43/41/10/240_F_43411016_AUDM5vX4DYFZNdsfA8KIfOQpS2Qq4Q14.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t4.ftcdn.net/jpg/03/57/74/77/240_F_357747756_L2pGrjInTFCGUovZIvww1hejL2UWgQRs.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t4.ftcdn.net/jpg/05/19/69/27/240_F_519692779_aYELMhe7YLC6qy8hhTkZ2GJcvl0Sdkpk.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t4.ftcdn.net/jpg/02/53/36/81/240_F_253368189_kJT28OQHrx9FBpRWLmuFeD14fZGvsh2T.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t4.ftcdn.net/jpg/04/46/08/29/240_F_446082907_14kBnXEZihKcDg4FuYFVJyzizrrpfXQk.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t3.ftcdn.net/jpg/07/26/61/20/240_F_726612015_Nwu9o7td8i6Hxgaw8KdH8ZgNRfQI0UsH.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t4.ftcdn.net/jpg/07/24/70/45/240_F_724704563_waqVe9izllqiGPSx4mxJNdfGeL9rtSLt.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t4.ftcdn.net/jpg/03/60/62/03/240_F_360620364_PSNHD7dy69NyxqunUIc4eugpEPn2yOuE.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-col p-4 border border-[#039a63] shadow-sm tra gap-2 rounded-lg">
+					<p className="flex justify-end">
+						<FaRegHeart />
+					</p>
+					<img
+						className="w-60 h-[200px] object-cover"
+						src="https://t3.ftcdn.net/jpg/01/37/70/00/240_F_137700093_M4rFblZNkuy9K55a8Mv0JVXOXeB5IAGV.jpg"
+						alt=""
+					/>
+					<p className="text-center">Product Name</p>
+					<div className="flex justify-between">
+						<p>Price</p>
+						<p>Weight</p>
+					</div>
+					<div className="flex gap-4 justify-center">
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">Buy</button>
+						<button className="border-2 w-1/2 px-2 py-1 rounded-md hover:border hover:border-[#039a63]">
+							Add to Cart
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Grocery
